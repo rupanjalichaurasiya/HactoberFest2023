bae72b5c791440da1d1f60c5950b38d39e62a98f
rupanjalichaurasiya <110722368+rupanjalichaurasiya@users.noreply.github.com>
2023-10-26 09:32:32 +0530

    Create operators.cpp

---
+++ operators.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+class Complex {
+private:
+    double real;
+    double imaginary;
+public:
+    Complex(double r, double i) : real(r), imaginary(i) {}
+
+    Complex operator+(const Complex& other) const {
+        return Complex(real + other.real, imaginary + other.imaginary);
+    }
+
+    Complex operator-(const Complex& other) const {
+        return Complex(real - other.real, imaginary - other.imaginary);
+    }
+
+    Complex operator*(const Complex& other) const {
+        double newReal = real * other.real - imaginary * other.imaginary;
+        double newImaginary = real * other.imaginary + imaginary * other.real;
+        return Complex(newReal, newImaginary);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Complex& complex) {
+        os << complex.real << " + " << complex.imaginary << "i";
+        return os;
+    }
+};
+
+int main() {
+    Complex num1(2.5, 3.7);
+    Complex num2(1.6, 6.2);
+
+    Complex sum = num1 + num2;
+    Complex difference = num1 - num2;
+    Complex product = num1 * num2;
+
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Difference: " << difference << std::endl;
+    std::cout << "Product: " << product << std::endl;
+
+    return 0;
+}
